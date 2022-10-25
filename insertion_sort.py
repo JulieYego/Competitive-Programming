2d0de30ab9d9017bf84000cc025170778a8ad81f
Julie Yego <julieyego@strathmore.edu>
2022-10-26 01:58:23 +0300

    insertion_sord_modify

--- insertion_sort.py
+++ insertion_sort.py
@@ -14,24 +14,33 @@ import sys
 #  2. INTEGER_ARRAY arr
 #
 
+
 def insertionSort1(n, arr):
     # Write your code here
-    #[2(0),4(1),6(2),8(3),3(4)] n = 5
-    x = arr[n-1]
-    new_arr = []
+    # [2(0),4(1),6(2),8(3),3(4)] n = 5
+    x = arr[n - 1]
     for i in range(n - 2, -1, -1):  # i = 3,2,1,0
-        if  arr[i] > x:
+        if arr[i] > x:
             arr[i + 1] = arr[i]
             print(*arr)
-            
+
         if arr[i] < x:
             arr[i + 1] = x
             print(*arr)
+            return
 
-if __name__ == '__main__':
-    n = int(input().strip())
+    # if __name__ == "__main__":
+    # n = int(input().strip())
 
-    arr = list(map(int, input().rstrip().split()))
+    # arr = list(map(int, input().rstrip().split()))
+    # arr = 1 3 5 9 13 22 27 35 46 51 55 83 87 23
 
-    insertionSort1(n, arr)
 
+# n = int(input("Enter size of array: ").strip())
+n = 10
+print(n)
+
+# arr = list(map(int, input().rstrip().split()))
+arr = [2, 3, 4, 5, 6, 7, 8, 9, 10, 1]
+print(arr)
+insertionSort1(n, arr)
